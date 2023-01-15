30c168bd96b8ac7eb12b7ac43e28849b4ed3a102
RAO YANG <lightrao@hotmail.com>
2023-01-16 07:27:30 +0800

    Adding a POST Route & Using Postman

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const placesRoutes = require("./routes/places-routes");
 
 const app = express();
 
+app.use(bodyParser.json());
+
 // outsource routes into separate files
 app.use("/api/places", placesRoutes); // => /api/places/...
 
