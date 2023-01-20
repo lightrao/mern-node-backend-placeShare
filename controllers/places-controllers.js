13a3f58e65e3efa6fe3a3a72b409f26ab110e0dc
RAO YANG <lightrao@hotmail.com>
2023-01-20 21:58:33 +0800

    Getting Places by the PlaceID

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -41,19 +41,28 @@ let DUMMY_PLACES = [
   },
 ];
 
-const getPlaceById = (req, res, next) => {
+const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid; // req.params => { pid : "p1" }
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
+
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not find a place!",
+      500
+    );
+    return next(error);
+  }
   if (!place) {
-    // Synchronous code
-    throw new HttpError(
+    // asynchronous code
+    const error = new HttpError(
       "Could not find a place for the provided place id!",
       404
     );
+    return next(error);
   }
-  res.json({ place: place });
+  res.json({ place: place.toObject({ getters: true }) });
 };
 
 const getPlacesByUserId = (req, res, next) => {
