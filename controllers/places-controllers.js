60b9dd1a55b75b42208e473c31c2719aea8083c4
RAO YANG <lightrao@hotmail.com>
2023-01-21 12:29:54 +0800

    Deleting Places

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -166,12 +166,28 @@ const updatePlace = async (req, res, next) => {
   res.status(200).json({ place: place.toObject({ getters: true }) });
 };
 
-const deletePlace = (req, res, next) => {
+const deletePlace = async (req, res, next) => {
   const placeId = req.params.pid;
-  if (!DUMMY_PLACES.find((p) => p.id === placeId)) {
-    throw new HttpError("Could not find the place for that id!", 404);
+
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not find the place for deleting!",
+      500
+    );
+    return next(error);
   }
-  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+  try {
+    await place.remove();
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong when deleting place!",
+      500
+    );
+    return next(error);
+  }
   res.status(200).json({ message: "Place deleted." });
 };
 
