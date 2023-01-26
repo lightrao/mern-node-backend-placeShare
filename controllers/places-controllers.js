c1dd29101e3f2e3f09281999f82f8c84912feffe
RAO YANG <lightrao@hotmail.com>
2023-01-26 10:57:05 +0800

    Getting Places - An Alternative

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -70,10 +70,11 @@ const getPlaceById = async (req, res, next) => {
 
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  // In Mongoose, Find doesn't provide a cursor, but directly an array.
-  let places;
+
+  // let places;
+  let userWithPlaces;
   try {
-    places = await Place.find({ creator: userId });
+    userWithPlaces = await User.findById(userId).populate("places");
   } catch (err) {
     console.log(err);
     const error = new HttpError(
@@ -83,14 +84,16 @@ const getPlacesByUserId = async (req, res, next) => {
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     // Asynchronous code
     return next(
       new HttpError("Could not find places for the provided user id!", 404)
     );
   }
   res.json({
-    places: places.map((place) => place.toObject({ getters: true })),
+    places: userWithPlaces.places.map((place) =>
+      place.toObject({ getters: true })
+    ),
   });
 };
 
