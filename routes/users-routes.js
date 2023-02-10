c40afbf900d7eaf35768718e993be156bde48d30
RAO YANG <lightrao@hotmail.com>
2023-02-10 08:07:25 +0800

    Filtering Files on the Backend (Images Only!)

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const usersControllers = require("../controllers/users-controllers");
+const fileUpload = require("../middleware/file-upload");
 
 const router = express.Router();
 
@@ -9,6 +10,9 @@ router.get("/", usersControllers.getUsers);
 
 router.post(
   "/signup",
+  // single get a middleware to retrieve a single file,
+  // specify the name in the to be expected body of the incoming request which hold the image or the file that should be extracted
+  fileUpload.single("image"),
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
